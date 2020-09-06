66ef5c2dc6fcfd8b93eb419e900e67c93138333e
YiranH <huangyr09@tamu.edu>
2020-09-05 19:24:40 -0500

    finished extension to open a google tab

--- js/background.js
+++ js/background.js
@@ -1,11 +1,3 @@
-// open a google search tab
-function search(query) { 
-    window.open(
-      "http://google.com/search?q=" + query,
-      "_blank"  
-    );
-}
-
-function main() {
-  search(456);
-}
+chrome.runtime.onInstalled.addListener(function () {
+  console.log("Hello World!");
+});
---
+++ js/popup.js
@@ -0,0 +1,13 @@
+// open a google search tab
+function search(query) {
+    chrome.tabs.create({
+        url: "http://google.com/search?q=" + query,
+        active: false
+    });
+}
+
+function start() {
+    search("random");
+};
+
+document.getElementById('start').addEventListener('click', start);
