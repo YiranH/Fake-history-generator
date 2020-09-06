39cfc2d6287ae57740cc3646051d8fef3a003b06
jackcspn <liupeixin111@sina.com>
2020-09-06 00:43:09 -0500

    record tab id, update on one tab

--- js/background.js
+++ js/background.js
@@ -1,16 +1,49 @@
+var tabId = -1;
+
+// open a google search tab
+function search(query) {
+    chrome.tabs.create(
+        {
+            url: "http://google.com/search?q=" + query,
+            active: false
+        },
+        function (tab) {
+            tabId = tab.id;
+        }
+    );
+}
+
 chrome.runtime.onInstalled.addListener(function () {
     console.log("Hello World!");
 });
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     console.log(message.type);
-    if (message.type === 'updateTab') {
-        chrome.tabs.update(
-            sender.tab.id,
-            {
-                url: message.link,
-                active: false
-            }
-        );
+    switch(message.type) {
+        // create new tab and search
+        case 'start':
+            search('123456');
+            break;
+        // jump to search result
+        case 'jumpResult':
+            chrome.tabs.sendMessage(
+                tabId, 
+                {
+                    type:"jumpResult"
+                }
+            );
+            break;
+        // update tab by url
+        case 'updateTab':
+            chrome.tabs.update(
+                tabId,
+                {
+                    url: message.link,
+                    active: false
+                }
+            );
+            break;
     }
+    console.log('tabId' + tabId);
 });
+
--- js/popup.js
+++ js/popup.js
@@ -1,27 +1,16 @@
-// open a google search tab
-function search(query) {
-    chrome.tabs.create({
-        url: "http://google.com/search?q=" + query,
-        active: false
-    });
-}
-
 function start() {
-    search("random");
+    chrome.runtime.sendMessage({
+        type: 'start'
+    });
+    // search("random");
 };
 
-// tell content to jump to result
+// jump to result
 function jumpResult() {
-    chrome.tabs.query(
-        {active: true, currentWindow: true}, 
-        function(tabs) {
-            chrome.tabs.sendMessage(
-                tabs[0].id, 
-                {
-                    type:"jumpResult"
-                }
-        );
+    chrome.runtime.sendMessage({
+        type: 'jumpResult'
     });
+    
 }
 
 document.getElementById('start').addEventListener('click', start);
